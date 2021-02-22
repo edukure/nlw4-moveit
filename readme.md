4f397e5ad7a6954be3c579678543d26673bdef35
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-02-22 18:46:58 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Move it
+
+Aplicação desenvolvida durante a Next Level Week 4 da Rocketseat
+
+## Stack
+- Nextjs
+- Tailwindcss
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="flex justify-center items-center">Hello World</h1>
+    </div>
+  );
+}
