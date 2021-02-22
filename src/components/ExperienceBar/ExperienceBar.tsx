00c4c5b767a9d815744c835ca75c488ae0282186
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-02-22 19:58:38 -0300

    feat: add current experience indicator

--- src/components/ExperienceBar/ExperienceBar.tsx
+++ src/components/ExperienceBar/ExperienceBar.tsx
@@ -1,21 +1,27 @@
-
 type ExperiencebarProps = {
     currentExp: number;
     maxExp: number;
-}
+};
 
-const ExperienceBar: React.FC<ExperiencebarProps> = ({currentExp, maxExp}) => {
-    const percentage = Math.floor(currentExp/maxExp*100);
+const ExperienceBar: React.FC<ExperiencebarProps> = ({ currentExp, maxExp }) => {
+    const percentage = Math.floor((currentExp / maxExp) * 100);
 
-    return (<div className="flex items-center justify-center min-w-full">
-        <span className="text-right px-2">0 xp</span>
-        <div className="flex flex-grow max-w-screen-md h-1 bg-gray-200 rounded-full overflow-hidden ">
-            <div className="h-full bg-green-400" style={{width: `${percentage}%`}}>
-                
+    return (
+        <div className="flex items-center justify-center min-w-full">
+            <span className="text-right px-3">0 xp</span>
+            <div className="flex flex-grow max-w-screen-md h-2 relative">
+                <div className="w-full h-full bg-gray-200 rounded-full overflow-hidden">
+                    <div className="h-full bg-green-400" style={{ width: `${percentage}%` }} />
+                    <span
+                        className="text-gray-500 absolute transform -translate-x-1/2 top-2"
+                        style={{ left: `${percentage}%` }}>
+                        {`${currentExp} xp`}
+                    </span>
+                </div>
             </div>
+            <span className=" text-left px-3">{`${maxExp} xp`}</span>
         </div>
-        <span className=" text-left px-2">{maxExp} xp</span>
-    </div>);
+    );
 };
 
 export default ExperienceBar;
